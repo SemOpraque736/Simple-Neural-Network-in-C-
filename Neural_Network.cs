3cbaee812b4e6e6a8dfac3ee514e9f8ddde2ebd6
SemOpraque736 <antoniomortari3510@gmail.com>
2025-09-21 01:08:37 -0300

    Neural_Network.cs

--- Neural_Network.cs
+++ Neural_Network.cs
@@ -1,55 +1,97 @@
 using System;
-
+using System.Runtime;
 class Program
 {
     static void Main(string[] args)
     {
+        // define variaveis
         float saida = 0;
+        // definindo o random
         Random random = new Random();
         var epochs = 200;
         var LR = 0.0008f;
-        int[] entradas = new int[3];
-        for (int i = 0; i < entradas.Length; i++)
+        // definindo dados de entradas
+        (int, int)[] entradas = new (int, int)[]
         {
-            entradas[i] = random.Next(0, 3);
-        }
-        int[] saidas = new int[3];
-        for (int i = 0; i < saidas.Length; i++)
+            (0, 0),
+            (0, 1),
+            (1, 0),
+            (1, 1),
+        };
+        // definindo dados de saidas
+        int[] saidas = new int[]{
+            0,
+            1,
+            1,
+            0
+        };
+        // definindo pesos 1 e 2
+        float[] pesos1 = new float[8];
+        for (int i = 0; i < pesos1.Length; i++)
         {
-            saidas[i] = random.Next(0, 3);
+            pesos1[i] = (float)(random.NextDouble() * -0.1 - 0.1);
+            Console.WriteLine("Peso " + (i + 1) + ": " + pesos1[i]);
         }
-        float[] pesos = new float[8];
-        for (int i = 0; i < pesos.Length; i++)
+        float[] pesos2 = new float[8];
+        for (int i = 0; i < pesos2.Length; i++)
         {
-            pesos[i] = (float)(random.NextDouble() * -0.1 - 0.1);
-            Console.WriteLine("Peso " + (i + 1) + ": " + pesos[i]);
+            pesos2[i] = (float)(random.NextDouble() * -0.1 - 0.1);
+            Console.WriteLine("Peso " + (i + 1) + ": " + pesos2[i]);
         }
+        // definindo bias
         float b1 = (float)(random.NextDouble() * -0.1 - 0.1);
-        for (int epoch = 0; epoch < epochs; epoch++)
+        // iniciando o treinamento
+        var rodando = true;
+        while (rodando)
         {
-            int Escolhido = random.Next(0, entradas.Length);
-            int input = entradas[Escolhido];
-            Escolhido = random.Next(0, saidas.Length);
-            int target = saidas[Escolhido];
-            saida = 0;
-            float erro = 0;
-            for (int Forward = 0; Forward < pesos.Length; Forward++)
+            for (int epoch = 0; epoch < epochs; epoch++)
             {
-                saida += pesos[Forward] * input;
-            }
-            saida += b1;
-            erro = target - saida;
-            for (int p = 0; p < pesos.Length; p++)
+                int Escolhido = random.Next(0, entradas.Length);
+                List<int> input = new List<int>
             {
-                pesos[p] += (float)LR * erro * input;
+                entradas[Escolhido].Item1,
+                entradas[Escolhido].Item2
+            };
+                Console.WriteLine(input);
+                Escolhido = random.Next(0, saidas.Length);
+                int target = saidas[Escolhido];
+                saida = 0;
+                float erro = 0;
+                for (int Forward = 0; Forward < pesos1.Length; Forward++)
+                {
+                    saida += pesos1[Forward] * input[0];
+                }
+                for (int Forward = 0; Forward < pesos2.Length; Forward++)
+                {
+                    saida += pesos2[Forward] * input[1];
+                }
+                saida += b1;
+                erro = target - saida;
+                for (int p = 0; p < pesos1.Length; p++)
+                {
+                    pesos1[p] += (float)LR * erro * input[0];
+                    pesos1[p] += (float)LR * erro * input[1];
+                }
+                for (int p = 0; p < pesos2.Length; p++)
+                {
+                    pesos2[p] += (float)LR * erro * input[0];
+                    pesos2[p] += (float)LR * erro * input[1];
+                }
                 b1 += (float)LR * erro;
+                Console.WriteLine($"Epoch: {epoch + 1}, Input: ({input[0]}, {input[1]}), Target: {target}, OutPut: {saida}, Error: {erro}");
             }
-            Console.WriteLine($"Epoch: {epoch + 1}, Input: {input}, Target: {target}, OutPut: {saida}, Error: {erro}");
-
-        }
-        for (int i = 0; i < pesos.Length; i++)
+        Console.WriteLine("Pesos Finais:");
+        Console.WriteLine("Pesos da entrada 1:");
+        for (int i = 0; i < pesos1.Length; i++)
         {
-            Console.WriteLine("Peso " + (i + 1) + ": " + pesos[i]);
+            Console.WriteLine("Peso " + (i + 1) + ": " + pesos1[i]);
+        }
+        Console.WriteLine("Pesos da entrada 2:");
+        for (int i = 0; i < pesos2.Length; i++)
+        {
+            Console.WriteLine("Peso " + (i + 1) + ": " + pesos2[i]);
+        }
+
         }
     }
 }
